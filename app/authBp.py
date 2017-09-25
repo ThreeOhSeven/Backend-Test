e161b7a5a0a763c4719324598700803abbe36f21
exponentialbit1024 <shah255@purdue.edu>
2017-09-24 20:37:05 -0400

    error fix

--- app/authBp.py
+++ app/authBp.py
@@ -19,19 +19,19 @@ def checkLogin():
 	if request.method == 'POST':
 		payload = json.loads(request.data.decode())
 		token = payload['authToken']
-		# try:
-		idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
-		print(idinfo)
-	    # idinfo = id_token.verify_oauth2_token(token, requests.Request())
-	    # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
-	    #     raise ValueError('Could not verify audience.')
-		if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-			raise ValueError('Wrong issuer.')
+		try:
+			idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+			print(idinfo)
+		    # idinfo = id_token.verify_oauth2_token(token, requests.Request())
+		    # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+		    #     raise ValueError('Could not verify audience.')
+			if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+				raise ValueError('Wrong issuer.')
 
-	    # ID token is valid. Get the user's Google Account ID from the decoded token.
-		userid = idinfo['sub']
-		return jsonify({'result' : True, 'selfToken' : "askdjaksdjqoiwuoqiuwjkleaksd"})
-		# except ValueError:
-		    # Invalid token
-			# return jsonify({'result' : False, 'error' : "Invalid token"})
+		    # ID token is valid. Get the user's Google Account ID from the decoded token.
+			userid = idinfo['sub']
+			return jsonify({'result' : True, 'selfToken' : "askdjaksdjqoiwuoqiuwjkleaksd"})
+		except ValueError:
+		    Invalid token
+			return jsonify({'result' : False, 'error' : "Invalid token"})
 	return jsonify({'result' : False, 'error' : "Invalid request"})
